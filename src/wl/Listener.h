256a37a107dff05eb39070b2d7ff714e663d39f1
Carlo Wood <carlo@alinoe.com>
2025-03-14 20:45:56 +0100

    Add wl::Listener and tests/signal-server.cxx

--- src/wl/Listener.h
+++ src/wl/Listener.h
@@ -1,18 +1,84 @@
 #pragma once
 
+#include "utils/has_print_on.h"
+#include "utils/print_pointer.h"
 #include <wayland-server-core.h>
+#include <utility>
 
 namespace wl {
+// This class defines a print_on method.
+using utils::has_print_on::operator<<;
 
-class Listener : public wl_listener
+template<typename Data>
+class EventType
 {
  public:
-  void register_with(struct wl_signal& signal, int)
-  {
+  static constexpr bool one_shot = false;
 
-     //   server.new_input.notify = server_new_input;
-     //   wl_signal_add(&server.backend->events.new_input, &server.new_input);
-  }
+ private:
+  Data const* data_;
+
+ public:
+  EventType(Data const* data) : data_(data) { }
+
+#ifdef CWDEBUG
+  void print_on(std::ostream& os) const;
+#endif
 };
+
+#ifdef CWDEBUG
+template<typename Data>
+void EventType<Data>::print_on(std::ostream& os) const
+{
+  os << '{';
+  os << "data_:" << utils::print_pointer(data_);
+  os << '}';
+}
+#endif
+
+struct SignalServerListener
+{
+  wl_listener listener_;
+};
+
+template<typename Data>
+class Listener : protected SignalServerListener, public events::Server<EventType<Data>>
+{
+ public:
+  using event_type_t = EventType<Data>;
+
+ private:
+  wl_signal* signal_ptr_{};
+
+ public:
+  Listener() = default;
+  Listener(wl_signal* signal_ptr) { init(); }
+
+  void init(wl_signal* signal_ptr);
+
+ private:
+  static void callback_(wl_listener* listener, void* data);
+};
+
+template<typename Data>
+void Listener<Data>::init(wl_signal* signal_ptr)
+{
+  signal_ptr_ = signal_ptr;
+
+  listener_.notify = &Listener<Data>::callback_;
+  wl_signal_add(signal_ptr_, &listener_);
+}
+
+//static
+template<typename Data>
+void Listener<Data>::callback_(wl_listener* listener, void* data_ptr)
+{
+  SignalServerListener* base = wl_container_of(listener, std::declval<SignalServerListener*>(), listener_);
+  Listener<Data>* self = static_cast<Listener<Data>*>(base);
+
+  Data* data = reinterpret_cast<Data*>(data_ptr);
+  EventType<Data> event_type(data);
+  self->trigger(event_type);
+}
 
 } // namespace wl
---
+++ tests/signal-server.cxx
@@ -0,0 +1,58 @@
+#include "sys.h"
+#include "events/Events.h"
+#include "wl/Listener.h"
+#include "wayland-server-core.h"
+#include "debug.h"
+
+extern "C" {
+struct wl_xyz_type {
+  int magic_{0x123456};
+};
+
+// wl_data_type
+wl_signal xyz_event;
+} // extern "C"
+
+std::ostream& operator<<(std::ostream& os, wl_xyz_type const& xyz)
+{
+  os << '{';
+  os << "magic_:0x" << std::hex << xyz.magic_;
+  os << '}';
+  return os;
+}
+
+using new_input_listener_t = wl::Listener<wl_xyz_type>;
+
+struct tinywl_server
+{
+  new_input_listener_t new_input_;
+
+  // Callback for new_input_.
+  void new_input(new_input_listener_t::event_type_t const& device)
+  {
+    DoutEntering(dc::notice, "tinywl_server::new_input(" << device << ")");
+  }
+};
+
+int main()
+{
+  Debug(NAMESPACE_DEBUG::init());
+
+  Dout(dc::notice, "This is signal-server!");
+
+  tinywl_server server;
+
+  auto handle = server.new_input_.request(server, &tinywl_server::new_input);
+
+  // External trigger happening.
+  {
+    wl_signal_init(&xyz_event);
+    server.new_input_.init(&xyz_event);      // Can only do this after xyz_event was initialized.
+
+    wl_xyz_type actual_data;
+    wl_signal_emit(&xyz_event, &actual_data);
+  }
+
+  // Clean up event registration.
+  handle.cancel();
+}
